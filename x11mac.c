5f13a319651bc8cf224f4674da1cd6aa8d0a1813
Trammell Hudson <hudson@trmm.net>
2014-01-29 03:37:55 +0000

    x colormap fixes; looks good!

--- x11mac.c
+++ x11mac.c
@@ -17,6 +17,7 @@
 #define VRAM_WIDTH 512
 #define VRAM_HEIGHT 384
 
+static uint8_t threshold = 0x80;
 
 void
 fb_copy(
@@ -25,19 +26,22 @@ fb_copy(
 )
 {
 	const XWDColor * const xfb_colors
-		= ((const XWDColor*) xfb) + be32toh(xfb->header_size);
-/*
+		= (const void*)((uintptr_t) xfb) + be32toh(xfb->header_size);
 	const uint8_t * const xfb_data
-		= (const uint8_t*)(xfb_colors + be32toh(xfb->ncolors)) + 0x120;
-*/
+		= (const uint8_t*)(xfb_colors + be32toh(xfb->ncolors));
+/*
 	// \todo: What is the correct offset?
 	const uint8_t * const xfb_data
 		= ((const uint8_t*)xfb_colors) + be32toh(xfb->ncolors)+0x20;
+*/
 	const size_t line_size = be32toh(xfb->bytes_per_line);
 
 	static int first;
 	if (first++ == 0)
-		printf("offset %zx\n", xfb_data - (const uint8_t*) xfb);
+		printf("offset %zx %zx\n",
+			xfb_data - (const uint8_t*) xfb,
+			((const uint8_t*)xfb_colors) - (const uint8_t*) xfb
+		);
 
 	//memset(fb, 0, VRAM_WIDTH*VRAM_HEIGHT/8);
 
@@ -52,11 +56,35 @@ fb_copy(
 
 			for (int x2 = 0 ; x2 < 8 ; x2++)
 			{
-				const uint8_t p = xfb_row[x + x2];
-				if (p)
-					pix = (pix >> 1) | 0x80;
+				uint8_t p = xfb_row[(x + x2)];
+#if 0
+				const XWDColor * const c = &xfb_colors[p];
+				const uint16_t r = c->red;
+				const uint16_t g = c->green;
+				const uint16_t b = c->blue;
+				const uint8_t n1 = be32toh(c->pixel);
+				const uint8_t n2 = (r+g+b) / (3*256);
+				if (x == 128 && x2 == 0 && y == 100)
+					printf("%08x %08x %04x %04x %04x => %02x\n",
+						p,
+						n1,
+						r,
+						g,
+						b,
+						n2
+					);
+#else
+				// fixed scale?
+				if (p == 1)
+					p = 0xFF;
 				else
-					pix >>= 1;
+				if (p != 0)
+					p = p - 1;
+#endif
+			
+				pix >>= 1;
+				if (p > threshold)
+					pix |= 0x80;
 			}
 
 			fb_row[x / 8] = pix;
@@ -122,12 +150,16 @@ main(
 	const char * const win_name = (const char *)(xfb + 1);
 	const size_t line_size = be32toh(xfb->bytes_per_line);
 
-	printf("XWD '%s' %dx%dx%d, %d colors, %zu bytes per line\n",
+	printf("XWD '%s' fmt=%d.%d.%d %dx%dx%d, %d colors @ %d, %zu bytes per line\n",
 		win_name,
+		be32toh(xfb->file_version),
+		be32toh(xfb->pixmap_format),
+		be32toh(xfb->visual_class),
 		be32toh(xfb->pixmap_width),
 		be32toh(xfb->pixmap_height),
 		be32toh(xfb->pixmap_depth),
 		be32toh(xfb->ncolors),
+		be32toh(xfb->byte_order),
 		line_size
 	);
 
